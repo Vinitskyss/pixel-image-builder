8d0eff61a43cb432bb283dae9d3341b4676bffbd
Vinitskyss <sergey.h12@gmail.com>
2020-04-17 00:51:25 +0700

    minor improvement

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
-pip3 install -r requirements.txt > /dev/null
+#!/bin/bash
 FILE=cpp/a.out
+PILLOW=$(pip3 list --format columns | grep Pillow)
+NUMPY=$(pip3 list --format columns | grep numpy)
+if [[ ! "$PILLOW" ]] || [[ ! "$NUMPY" ]] ;
+then
+    pip3 install -r requirements.txt > /dev/null
+fi
 g++ ./cpp/main.cpp
 python3 getImagePixels.py | ./a.out | python3 assembleImage.py
